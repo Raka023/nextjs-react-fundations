b06f7f93ff5f120ab3445ee0ae6ca9a2f6f60c30
RaZhK_ <rakaputrautama23@outlook.com>
2025-07-21 16:01:02 +0700

    some changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,16 @@
 import LikeButton from "./like-button"
 
 function Header({ title }) {
-    return <h1>{title ? title : 'Default title'}</h1>
+    return <h1>{title ? title : 'Title'}</h1>
 }
 
 export default function HomePage() {
     
-    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
+    const names = ['Jojo', 'Abdul', 'Andy']
 
     return (
         <div>
-            <Header title="Develop. Preview. Ship." />
+            <Header title="Persons" />
             <ul>
                 {names.map(name => <li key={name}>{name}</li>)}
             </ul>
